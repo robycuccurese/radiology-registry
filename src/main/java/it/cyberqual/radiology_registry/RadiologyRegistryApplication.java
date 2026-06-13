1eb295666e194e10d74f41d8036eb4f05fd5af9d
Roberto Cuccurese <roberto.cuccurese@eng.it>
2026-06-13 18:58:54 +0200

    first commit

---
+++ src/main/java/it/cyberqual/radiology_registry/RadiologyRegistryApplication.java
@@ -0,0 +1,13 @@
+package it.cyberqual.radiology_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiologyRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadiologyRegistryApplication.class, args);
+	}
+
+}
